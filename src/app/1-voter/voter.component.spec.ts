181d081f5b136b8745648d2e5cb937a2b5b254aa
Dmitri Kondramachine <dmkon@hotmail.com>
2018-01-31 11:36:05 -0500

    Initial commit.

---
+++ src/app/1-voter/voter.component.spec.ts
@@ -0,0 +1,44 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { VoterComponent } from './voter.component';
+
+describe('VoterComponent', () => {
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VoterComponent ]
+    });
+
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
+    // fixture.nativeElement
+    // fixture.debugElement
+  });
+
+  it('it should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    const de = fixture.debugElement.query(By.css('.vote-count'));
+    const el: HTMLElement = de.nativeElement;
+
+    expect(el.innerText).toContain(21);
+  });
+
+  it('it should highlight the upvote button if I have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    const de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    expect(de.classes['highlighted']).toBeTruthy();
+  });
+
+  it('it should increase total votes when I click upvote button', () => {
+    const button = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+    button.triggerEventHandler('click', null);
+    expect(component.totalVotes).toBe(1);
+  })
+});
